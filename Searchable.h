4c632a848eb9dded9a9bb1a2d439263d59568c0b
Dor Gamliel <dorgamliel@gmail.com>
2020-01-14 20:08:35 +0200

    Adding header filed

---
+++ Searchable.h
@@ -0,0 +1,14 @@
+//
+// Created by dorgamliel on 14/01/2020.
+//
+
+#ifndef TEMPFORSERVER_SEARCHABLE_H
+#define TEMPFORSERVER_SEARCHABLE_H
+
+
+class Searchable {
+
+};
+
+
+#endif //TEMPFORSERVER_SEARCHABLE_H
---
+++ Searcher.h
@@ -0,0 +1,14 @@
+//
+// Created by dorgamliel on 14/01/2020.
+//
+
+#ifndef TEMPFORSERVER_SEARCHER_H
+#define TEMPFORSERVER_SEARCHER_H
+
+
+class Searcher {
+
+};
+
+
+#endif //TEMPFORSERVER_SEARCHER_H
