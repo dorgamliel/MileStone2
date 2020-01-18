7ac9804a18573fdf5528c1a4ceb7fc3b8feaa777
alon <alon_shoval@hotmail.com>
2020-01-18 23:18:33 +0200

    DFS test working with path printing to console added support for -1 cells added comments

--- DFS.h
+++ DFS.h
@@ -16,19 +16,43 @@ public:
       return DFSOper(start, searchable, visited);
   }
   vector<State<T>*> DFSOper(State<T>* state, Searchable<T>* searchable , map<T, bool> visited) {
+        // if reached target vertex, return the path from start vertex to target vertex
         if (searchable->isGoalState(state)) {
             return createPath(state);
         }
+        // mark this vertex as visited
         visited[state->getState()] = true;
         vector<State<T>*> allNeighbours = searchable->getNeighbours(state);
+        // if the neighbour list is empty, we have reached a dead end, and we will return empty vector
+        if (allNeighbours.empty()) {
+            vector<State<T>*> vec;
+            return vec;
+        }
+        // iterate through neighbours and try to find a path to target vertex
         for (auto s : allNeighbours) {
             T vertex = s->getState();
+            // if this neighbour has not been visited yet
             if (visited.find(vertex) == visited.end()) {
+                // set the total cost to get to this neighbour and current vertex to be neighbours parent
                 s->setCost(state->getCost() + searchable->getCost(state, s));
                 s->setCameFrom(state);
-                return DFSOper(s, searchable, visited);
+                // run DFS recursively on the neighbour vertex
+                auto path = DFSOper(s, searchable, visited);
+                // if the path is empty, it means we have reached a dead end, so we will try to find a path
+                // to target vertex from the next neighbour, otherwise, meaning we found a path from the neighbour
+                // to target vertex, we will return the total path from start vertex to target vertex
+                if (path.empty()) {
+                    continue;
+                } else {
+                    return path;
+                }
             }
         }
+        // reaching this code means current vertex has neighbours but they have all been visited in the past and
+        // therefore it is useless to visit them again, meaning we reached a dead end, therefore returning
+        // an empty path
+        vector<State<T>*> vec;
+        return vec;
 
 
 
@@ -50,6 +74,8 @@ public:
             }
         }*/
     }
+
+    //  iterate through vertex's parents in order to create a path from start vertex to target vertex
     vector<State<T>*> createPath(State<T> *goal) {
         vector<State<T> *> path;
         State<T>* s = goal;
--- MatrixSearchable.cpp
+++ MatrixSearchable.cpp
@@ -9,23 +9,40 @@
 
 vector<State<pair<int, int>>*> MatrixSearchable::getNeighbours(State<pair<int, int>>* s){
     vector<State<pair<int, int>>*> vector;
+    // location of current cell
     int i = s->getState().first;
     int j = s->getState().second;
-    if (i != this->size.first && (*costs)[i+1][j] != -1)  {
-        auto* s1 = new State<pair<int, int>>({i+1, j});
-        vector.push_back(s1);
+    /*
+     * create a list of all possible neighbours by this order:
+     * 1. neighbour below
+     * 2. neighbour above
+     * 3. neighbour to the right
+     * 4. neighbour to the left
+     * an adjacent cell with a cost of -1 is considered unreachable and therefore not considered a neighbour
+     */
+    if (i != this->size.first)  {
+        if ((*costs)[i+1][j] != -1) {
+            auto *s1 = new State<pair<int, int>>({i + 1, j});
+            vector.push_back(s1);
+        }
     }
     if (i != 0) {
-        auto* s2 = new State<pair<int, int>>({i-1, j});
-        vector.push_back(s2);
+        if ((*costs)[i-1][j] != -1) {
+            auto *s2 = new State<pair<int, int>>({i - 1, j});
+            vector.push_back(s2);
+        }
     }
     if (j != this->size.second) {
-        auto* s3 = new State<pair<int, int>>({i, j+1});
-        vector.push_back(s3);
+        if ((*costs)[i][j+1] != -1) {
+            auto *s3 = new State<pair<int, int>>({i, j + 1});
+            vector.push_back(s3);
+        }
     }
     if (j != 0) {
-        auto* s4 = new State<pair<int, int>>({i, j-1});
-        vector.push_back(s4);
+        if ((*costs)[i][j-1] != -1) {
+            auto *s4 = new State<pair<int, int>>({i, j - 1});
+            vector.push_back(s4);
+        }
     }
     return vector;
 }
@@ -38,12 +55,17 @@ vector<State<pair<int, int>>*> MatrixSearchable::getNeighbours(State<pair<int, i
     return vector.size();
 }*/
 
+// set the size of the matrix
 void MatrixSearchable::setSize(pair<int, int> si) {
     this->size = si;
 }
+
+// get cost of the edge from origin to dest
 double MatrixSearchable::getCost(State<pair<int, int>> *origin, State<pair<int, int>> *dest) {
     return (*costs)[dest->getState().first][dest->getState().second];
 }
+
+// set the costs table
 void MatrixSearchable::setCosts(vector<vector<int>>* c) {
     this->costs = c;
 }
