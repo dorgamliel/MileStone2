78d8a0adee3387d34c569e5ac4cbf99ed09e2668
Dor Gamliel <dorgamliel@gmail.com>
2020-01-20 17:15:44 +0200

    fixed BestFS and StringReverser

--- StringReverser.cpp
+++ StringReverser.cpp
@@ -5,14 +5,16 @@
 #include <cstring>
 #include "StringReverser.h"
 
-string StringReverser::solveProblem() {
-    this->problem = this->problem.substr(0, this->problem.find("\r"));
-    int n = this->problem.length();
-    string temp = this->problem;
+string* StringReverser::solveProblem() {
+    string* s = this->problem;
+    *s = s->substr(0, this->problem->find("\r"));
+    this->problem = s;
+    int n = this->problem->length();
+    string* temp = this->problem;
     for (int i=0; i < n/2;i++){
         swap(temp[i], temp[n-i-1]);
     }
     this->solution = temp;
     return this->solution;
 }
-void StringReverser::setProblem(string prob) { this->problem = prob; }
+void StringReverser::setProblem(string* prob) { this->problem = prob; }
